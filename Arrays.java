ac025604c119f5d554bf72563a470b05978fe1d7
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-07-17 18:01:44 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,4 @@
-1)subarray
+1)Pair
   public class pairs_in_array {
 
     public static  boolean reverse(int ar[]){
@@ -18,3 +18,52 @@
     
 }
 
+2)SubArray
+  public class SubArrays {
+    public static  boolean subarray(int ar[]){
+        int n=ar.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<=n-1;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.println(ar[k]);
+                }
+                 System.out.println();
+            }
+             System.out.println();
+        }
+        return true;
+        
+    }
+    public static void main(String[]args){
+        int a[]={1,2,3,4,5};
+        System.out.print(subarray(a));
+    }
+}
+
+
+3)MaxSubArraySum
+  public class MaxSubArraysSum {
+    public static  void subarray(int ar[]){
+        int currSum=0;
+        int MaxSum=Integer.MAX_VALUE;
+        int n=ar.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<=n-1;j++){
+                currSum=0;
+                for(int k=i;k<=j;k++){
+                    currSum+=ar[k];
+                    if(currSum<MaxSum){
+                        MaxSum=currSum;
+                    }
+                  System.out.println();
+                }
+                 System.out.println();
+            }
+         }
+        System.out.println(MaxSum);
+     }
+    public static void main(String[]args){
+        int a[]={1,2,3,4,5};
+        subarray(a);
+    }
+ }
