3f1a878a22faede418745caa2d60f3a870417ade
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-07-17 17:31:46 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,1 +1,20 @@
+1)subarray
+  public class pairs_in_array {
+
+    public static  boolean reverse(int ar[]){
+        int n=ar.length;
+        for(int i=0;i<=n-2;i++){
+            for(int j=0;j<=n-1;j++){
+            System.out.println("("+ ar[i]+","+ar[j]+")");
+            }
+        }
+        return true;
+        
+    }
+    public static void main(String[]args){
+        int a[]={1,2,3,4,5};
+        System.out.print(reverse(a));
+    }
+    
+}
 
