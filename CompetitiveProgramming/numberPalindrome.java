ab1cd0eb318439b73cb6d20d2207603d194ad7cc
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-07-23 17:42:15 +0530

    Create numberPalindrome.java
    LeetCode1_23-07-2023_Sunday

---
+++ CompetitiveProgramming/numberPalindrome.java
@@ -0,0 +1,32 @@
+public class PalinInte {
+    public static boolean isPalindrome(int x) {
+        int sum=0;
+        if(x<0){
+            return false;
+        }
+        // while(x>10){
+        //     int temp=x%10;
+        //     sum+=temp;
+        //     x=x/10;
+        //     }
+           while (x > 0 && x > 10) { 
+            int temp = x % 10;
+            sum += temp;
+            x = x / 10;
+        }
+        sum += x; 
+            String str =String.valueOf(sum);
+            String rev
+            = new StringBuilder(str).reverse().toString();
+            if((str).equals(rev)){
+                return true;
+
+            }
+            return false;
+        
+    }
+    public static void main (String[]args){
+        int num=12145;
+        System.out.println( isPalindrome(num));
+    }
+} 
