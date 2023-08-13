caf22630a687da7a75c4b68847d5e38a5551e249
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-08-13 22:13:01 +0530

    Create nomethodLengthOfString.java

---
+++ strings/nomethodLengthOfString.java
@@ -0,0 +1,13 @@
+class LengthOfString{
+    public static void main(String[] args) {
+        String Str ="Hello Friends";
+        int count =0;
+        
+        for(int i =0;i<Str.length();i++){
+        count ++;
+        }
+       System.out.println(count);
+
+    }
+
+}
