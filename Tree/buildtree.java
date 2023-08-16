615bab3bc5ae79d8ca255650b9ad7e473cb09e28
Achiver <106165315+Infix-X@users.noreply.github.com>
2023-08-16 22:50:18 +0530

    Create buildtree.java

---
+++ Tree/buildtree.java
@@ -0,0 +1,30 @@
+ 
+    static int MAX = 26;
+     
+    // Function to print the frequency
+    // of each of the characters of
+    // s in alphabetical order
+    static void compressString(String s, int n)
+    {
+        // To store the frequency
+        // of the characters
+        int freq[] = new int[MAX] ;
+     
+        // Update the frequency array
+        for (int i = 0; i < n; i++)
+        {
+            freq[s.charAt(i) - 'a']++;
+        }
+     
+        // Print the frequency in alphatecial order
+        for (int i = 0; i < MAX; i++)
+        {
+     
+            // If the current alphabet doesn't
+            // appear in the string
+            if (freq[i] == 0)
+                continue;
+     
+            System.out.print((char)(i + 'a') +""+ freq[i]);
+        }
+    }
